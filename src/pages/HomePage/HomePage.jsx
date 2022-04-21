1c45ae1a83fcac6acb694e58576a3ad92a1d53af
rodas007 <sergioproba@gmail.com>
2022-04-21 09:50:23 +0200

    ADD Home Page + components

---
+++ src/pages/HomePage/HomePage.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+
+export default function HomePage() {
+  return (
+    <div>
+    
+    
+    
+    
+    <h1>Esta es la home page</h1>
+    
+    
+    
+    
+    
+    
+    
+    </div>
+
+
+
+
+
+  )
+}
