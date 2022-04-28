6a41fb8b2cb3ad7ee8a3380871a1a6b217cae5a1
Juan Carlos Rojas Gamboa <rojas_jc@hotmail.com>
2022-04-28 17:06:09 +0200

    ficha

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import { PricesPage } from "./pages/PricesPage/PricesPage";
 import LoginPage from "./pages/LoginPage/LoginPage";
 import { RegisterPage } from "./pages/RegisterPage/RegisterPage";
 import ReserveCompletePage from "./pages/ReserveCompletePage/ReserveCompletePage";
+import FichaComponent from "./components/FichaComponent/FichaComponent";
 
 function App() {
   return (
@@ -22,6 +23,7 @@ function App() {
           <Route path="/login" element={<LoginPage />} />
           <Route path="/register" element={<RegisterPage />} />
           <Route path="/complete" element={<ReserveCompletePage />} />
+          <Route path="/ficha" element={<FichaComponent />} />
         </Routes>
       </div>
     </Router>
