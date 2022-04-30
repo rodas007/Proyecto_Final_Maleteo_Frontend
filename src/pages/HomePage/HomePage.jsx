8ff1b2b39bd1a67273cf0b3a11b1ed4aa2c77bbd
rodas007 <sergioproba@gmail.com>
2022-04-30 13:40:45 +0200

    add options select home

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -79,7 +79,11 @@ export default function HomePage() {
             </Link>
             <select placeholder="Nº de piezas" value="" className="retirada">
             
-            
+            <option value="1">1</option>
+                    <option value="2">2</option>
+                    <option value="3">3</option>
+                    <option value="4">4</option>
+                    <option value="5">5</option>
             
             
             </select>
